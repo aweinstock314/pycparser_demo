b0589b5f647f81685dee26c15909d08e784c4d61
Konstantinos Mitropoulos <mitros123@hotmail.com>
2018-03-21 15:04:30 +0000

    Added simple_test_structs_and_typedefs.c

---
+++ simple_test_structs_and_typedefs.c
@@ -0,0 +1,49 @@
+int x;
+int y[100];
+
+
+typedef struct boom
+{
+    int b;
+    int x;
+} bam;
+
+
+bam grenade;
+
+struct lol
+{
+    int a;
+    int b;
+    int* m;
+    char c[10];
+	char d[x];
+} *haha;
+
+struct lol hehe;
+
+
+
+
+int a_fun(int x,int* y)
+{
+    int b=1;
+    int *c;
+    struct lol m;
+    struct lol * n;
+
+	b=4;
+
+    return 0;
+}
+
+
+struct ll
+{
+    int a;
+};
+
+int z[10];
+
+struct lol * muahaha;
+
